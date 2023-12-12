01873837d6bcecd63cec7b7d38abce9ab117bb63
Ihsan Hadiansyach <ihadiansyach@gmail.com>
2023-12-12 11:23:32 +0700

    chore: change text with form field

--- lib/modules/dialog/dialog_share_signal.dart
+++ lib/modules/dialog/dialog_share_signal.dart
@@ -55,7 +55,7 @@ class _DialogShareSignalState extends State<DialogShareSignal> {
             width: 2,
           )),
       child: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
@@ -82,7 +82,8 @@ class _DialogShareSignalState extends State<DialogShareSignal> {
                       children: [
                         Text(
                           'Will be expired on $selectedFormattedDate',
-                          style: const TextStyle(color: Colors.black54, fontSize: 12),
+                          style: const TextStyle(
+                              color: Colors.black54, fontSize: 12),
                         ),
                         const Row(
                           mainAxisAlignment: MainAxisAlignment.start,
@@ -92,7 +93,7 @@ class _DialogShareSignalState extends State<DialogShareSignal> {
                             Text('Market reversal')
                           ],
                         ),
-                        const SizedBox(height: 4),
+                        const SizedBox(height: 16),
                         const Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
@@ -101,14 +102,21 @@ class _DialogShareSignalState extends State<DialogShareSignal> {
                             Text('XAAUSD')
                           ],
                         ),
-                        const SizedBox(height: 4),
+                        const SizedBox(height: 16),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             const SizedBox(width: 90, child: Text('Pairs')),
                             const SizedBox(width: 16),
-                            SizedBox(
-                              height: 25,
+                            Container(
+                              height: 35,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(
+                                    color: Colors.black26,
+                                  )),
+                              padding: const EdgeInsets.only(left: 8, right: 8),
                               child: DropdownButton<String>(
                                 value: selectedOption,
                                 items:
@@ -127,58 +135,85 @@ class _DialogShareSignalState extends State<DialogShareSignal> {
                             ),
                           ],
                         ),
-                        const SizedBox(height: 4),
+                        const SizedBox(height: 16),
                         const Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            SizedBox(width: 90, child: Text('Entry Price')),
+                            SizedBox(width: 90, child: Text('Entry Price',
+                            style: TextStyle(fontWeight: FontWeight.bold),)),
                             SizedBox(width: 16),
                             Text('')
                           ],
                         ),
-                        const SizedBox(height: 4),
+                        const SizedBox(height: 16),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             const SizedBox(width: 90, child: Text('TP Price')),
                             const SizedBox(width: 16),
                             Container(
-                                width: 100,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    border: Border.all(
-                                      color: Colors.black26,
-                                    )),
-                                padding: const EdgeInsets.only(left: 8, right: 8),
-                                child: const Text('1930.00')),
+                              height: 35,
+                              width: 130  ,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(
+                                    color: Colors.black26,
+                                  )),
+                              padding: const EdgeInsets.only(left: 8, right: 8),
+                              child: TextFormField(
+                                // TODO: Implement logic to handle TP price input.
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintStyle: TextStyle(
+                                      fontSize: 14
+                                  ),
+                                  hintText: 'Enter TP price',
+                                ),
+                              ),
+                            ),
                             const SizedBox(width: 16),
                             const Text('(+1000 Pips)')
                           ],
                         ),
-                        const SizedBox(height: 4),
+                        const SizedBox(height: 16),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             const SizedBox(width: 90, child: Text('SL Price')),
                             const SizedBox(width: 16),
                             Container(
-                                width: 100,
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(8),
-                                    border: Border.all(
-                                      color: Colors.black26,
-                                    )),
-                                padding: const EdgeInsets.only(left: 8, right: 8),
-                                child: const Text('XAAUSD')),
+                              height: 35,
+                              width: 130  ,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(
+                                    color: Colors.black26,
+                                  )),
+                              padding: const EdgeInsets.only(left: 8, right: 8),
+                              child: TextFormField(
+                                // TODO: Implement logic to handle TP price input.
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintStyle: TextStyle(
+                                      fontSize: 14
+                                  ),
+                                  hintText: 'Enter SL price',
+                                ),
+                              ),
+                            )
+,
                             const SizedBox(width: 16),
                             const Text('(-1000 Pips)')
                           ],
                         ),
-                        const SizedBox(height: 4),
+                        const SizedBox(height: 16),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
-                            const SizedBox(width: 90, child: Text('Expired Time')),
+                            const SizedBox(
+                                width: 90, child: Text('Expired Time')),
                             const SizedBox(width: 16),
                             Text(selectedFormattedDate),
                             const SizedBox(width: 16),
@@ -188,13 +223,31 @@ class _DialogShareSignalState extends State<DialogShareSignal> {
                             ),
                           ],
                         ),
-                        const SizedBox(height: 4),
-                        const Row(
+                        const SizedBox(height: 16),
+                        Row(
                           mainAxisAlignment: MainAxisAlignment.start,
                           children: [
                             SizedBox(width: 90, child: Text('Signal Price')),
                             SizedBox(width: 16),
-                            Text('\$10')
+                            Container(
+                              height: 35,
+                              width: 130,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  border: Border.all(
+                                    color: Colors.black26,
+                                  )),
+                              padding: const EdgeInsets.only(left: 8, right: 8),
+                              child: TextFormField(
+                                // TODO: Implement logic to handle TP price input.
+                                keyboardType: TextInputType.number,
+                                decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintStyle: TextStyle(fontSize: 14),
+                                  hintText: 'Enter Signal price',
+                                ),
+                              ),
+                            )
                           ],
                         ),
                       ],
